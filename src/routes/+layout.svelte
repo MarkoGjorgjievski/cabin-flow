f49e42611a81d20ae201e87dfd6729f2761a743f
MarkoGjorgji <marko.gjorgjievski@intertec.io>
2023-07-11 00:28:17 +0200

    active path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,64 +1,44 @@
 <script>
   import '../app.css';
-  import PlaneIcon from "../lib/components/atoms/icons/PlaneIcon.svelte";
+  import { page } from "$app/stores";
+  import { tick } from "svelte";
+  import Divider from "../lib/components/atoms/Divider.svelte";
+  import {
+    LogoIcon,
+    PlaneIcon,
+    ListIcon,
+    CakeIcon,
+    RectangleGroupIcon,
+    ClipboardIcon, ArrowsSquare
+  } from "../lib/components/atoms/icons";
 </script>
 
 <nav class='h-screen w-fit fixed top-0 flex h-full flex-col gap-2 justify-between bg-base-200 p-2'>
   <ul>
-    <li><a href='/'><PlaneIcon /></a></li>
+    <li><a href='/'><LogoIcon /></a></li>
   </ul>
   <ul>
-    <li><a href='/'><PlaneIcon /></a></li>
-    <li>
-      <a href='/positions'>
-        <PlaneIcon />
-      </a>
+    <li class:bg-primary={$page.url.pathname === '/'}><a href='/'><PlaneIcon /></a></li>
+    <li class:bg-primary={$page.url.pathname.includes('positions')}><a  href='/positions'><ListIcon /></a></li>
+
+    <Divider />
+
+    <li class:bg-primary={$page.url.pathname.includes('/business/meals')}><a href='/business/meals/FWD'><CakeIcon /></a></li>
+    <li class:bg-primary={$page.url.pathname.includes('/business/zones')}><a href='/business/zones/FWD'><RectangleGroupIcon /></a>
     </li>
 
-    <!--    <Divider />-->
-    <hr />
+    <!-- This is supposed to be: first zone, first position-> zones, position [0]   -->
+    <li class:bg-primary={$page.url.pathname.includes('/business/seat-chart')}><a href='/business/seat-chart/FWD/left'><ClipboardIcon /></a></li>
 
-    <li>
-      <a href='/business/meals/FWD'>
-        <PlaneIcon />
-      </a>
-    </li>
-    <li>
-      <a href='/business/zones/FWD'>
-        <PlaneIcon />
-      </a>
-    </li>
-    <!-- This is supposed to be: first zone, first position -> zones, position [0]   -->
-    <li>
-      <a href='/business/seat-chart/FWD/left'>
-        <PlaneIcon />
-      </a>
-    </li>
+    <Divider />
 
-    <!--    <Divider />-->
-    <hr />
-
-    <li>
-      <a href='/economy/meals/options'>
-        <PlaneIcon />
-      </a>
-    </li>
-    <li>
-      <a href='/economy/zones/service'>
-        <PlaneIcon />
-      </a>
-    </li>
-    <li>
-      <a href='/economy/360'>
-        <PlaneIcon />
-      </a>
-    </li>
+    <li class:bg-primary={$page.url.pathname.includes('/economy/meals')}><a href='/economy/meals/options'><CakeIcon /></a></li>
+    <li class:bg-primary={$page.url.pathname.includes('/economy/zones')}><a href='/economy/zones/service'><RectangleGroupIcon /></a></li>
+    <li class:bg-primary={$page.url.pathname.includes('/economy/360')}><a href='/economy/360'><ArrowsSquare /></a></li>
   </ul>
 
   <ul>
-    <li>
-      <a><PlaneIcon /></a>
-    </li>
+    <li><a><PlaneIcon /></a></li>
     <!--    <ThemeSwitcher />-->
   </ul>
 </nav>
